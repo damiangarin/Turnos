e32ec01b97c631fb0bdd5c6ea88d1231b723ccf7
damiangarin <108473053+damiangarin@users.noreply.github.com>
2022-06-30 15:51:57 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -17,9 +17,12 @@ contenedorAsignado.appendChild(persona)
 
 const checkearTurno = () => {
     // buscar dni en pacientes[{}]
-    const turno = pacientes.find(paciente => paciente.dni === inputDni.value).asignado
-    // persona.innerText = pacientes[0].asignado === true ? 'Turno asignado' : 'Turno no asignado'
-    persona.innerText = turno === true ? 'Turno asignado' : 'Turno no asignado'
+    const turno = pacientes.find(paciente => paciente.dni === inputDni.value)
+    if (turno) {        
+        persona.innerText = turno.asignado === true ? 'Turno asignado' : 'Turno no asignado'    
+    }else {
+        persona.innerText = 'DNI no registrado'
+    }
 }
 
 
